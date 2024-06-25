51a668a9c536859fe9a7a0e943fb4de715665790
Alexey Klyuchnikov <klyuchnikov.alexey@yandex.ru>
2024-06-25 20:37:27 +0300

    Delete List, Long; Add ConcurrentHashMap, AtomicLong

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -2,54 +2,43 @@ package ru.netology.repository;
 
 import ru.netology.model.Post;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 // Stub
 public class PostRepository {
-    private final List<Post> posts = new ArrayList<>();
-    private int postCounter;
+    private final ConcurrentHashMap<Long, Post> posts = new ConcurrentHashMap<>();
+    private final AtomicLong idCounter = new AtomicLong();
 
     public PostRepository() {
-        posts.addAll(List.of(
-                new Post(1, "First"),
-                new Post(2, "Second"),
-                new Post(3, "Third"),
-                new Post(4, "Fourth"),
-                new Post(5, "Fifth"),
-                new Post(6, "Sixth"),
-                new Post(7, "Seventh"),
-                new Post(8, "Eighth"),
-                new Post(9, "Ninth"),
-                new Post(10, "Tenth"),
-                new Post(11, "Eleventh")
-        ));
-        postCounter = posts.size();
+        posts.put(1L, new Post(1, "First"));
+        posts.put(2L, new Post(2, "Second"));
+        posts.put(3L, new Post(3, "Third"));
+        idCounter.getAndSet(posts.size());
     }
 
-    public List<Post> all() {
-        return posts;
+    public Collection<Post> all() {
+        return posts.values();
     }
 
     public Optional<Post> getById(long id) {
-        return posts.stream()
-                .filter(x -> x.getId() == id)
-                .findFirst();
+        return Optional.ofNullable(posts.get(id));
     }
 
     public Post save(Post post) {
-        Optional<Post> postSearch = posts.stream()
-                .filter(x -> x.getId() == post.getId())
-                .findFirst();
-        if (postSearch.isEmpty() || post.getId() == 0) {
-            post.setId(++postCounter);
-            posts.add(post);
+        if (post.getId() == 0) {
+            idCounter.incrementAndGet();
+            post.setId(idCounter.get());
+            posts.put(idCounter.get(), post);
+        } else {
+            posts.put(post.getId(), post);
         }
         return post;
     }
 
     public void removeById(long id) {
-        posts.removeIf(post -> post.getId() == id);
+        posts.remove(id);
     }
 }
--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -14,6 +14,8 @@ public class MainServlet extends HttpServlet {
     private static final String METHOD_GET = "GET";
     private static final String METHOD_POST = "POST";
     private static final String METHOD_DELETE = "DELETE";
+    private static final String PATH = "/api/posts";
+    private static final String PATH_ID = "/api/posts/\\d+";
 
     @Override
     public void init() {
@@ -29,29 +31,27 @@ public class MainServlet extends HttpServlet {
             final var path = req.getRequestURI();
             final var method = req.getMethod();
             // primitive routing
-            if (method.equals(METHOD_GET) && path.equals("/api/posts")) {
+            if (method.equals(METHOD_GET) && path.equals(PATH)) {
                 controller.all(resp);
                 return;
             }
-            if (method.equals(METHOD_GET) && path.matches("/api/posts/\\d+")) {
+            long id = Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
+            if (method.equals(METHOD_GET) && path.matches(PATH_ID)) {
                 // easy way
-                final var id = Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
                 controller.getById(id, resp);
                 return;
             }
-            if (method.equals(METHOD_POST) && path.equals("/api/posts")) {
+            if (method.equals(METHOD_POST) && path.equals(PATH)) {
                 controller.save(req.getReader(), resp);
                 return;
             }
-            if (method.equals(METHOD_DELETE) && path.matches("/api/posts/\\d+")) {
+            if (method.equals(METHOD_DELETE) && path.matches(PATH_ID)) {
                 // easy way
-                final var id = Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
                 controller.removeById(id, resp);
                 return;
             }
             resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
         } catch (Exception e) {
-            e.printStackTrace();
             resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }
     }
